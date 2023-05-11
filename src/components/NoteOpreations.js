eae2f5fc373828748317a7457bfd4d32adc87022
RasoulDebugger <rasoul.merzai123@gmail.com>
2023-05-11 12:18:44 +0500

    clear note value and desc

--- src/components/NoteOpreations.js
+++ src/components/NoteOpreations.js
@@ -25,6 +25,9 @@ export default function NoteOperations() {
     addDoc(dbInstance, {
       noteTitle: noteTitle,
       noteDess: noteDesc,
+    }).then(() => {
+      setNoteTitle("");
+      setNoteDesc("");
     });
   };
   return (
