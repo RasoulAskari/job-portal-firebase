95599f0a7fad612f62fab0cb9d9054335f0b633d
RasoulDebugger <rasoul.merzai123@gmail.com>
2023-05-12 00:47:22 +0500

    getSingle from firebase

--- src/components/NoteDetails.js
+++ src/components/NoteDetails.js
@@ -1,8 +1,20 @@
 "use client";
-import { app, database } from "../../firebaseConfig";
+import { useEffect } from "react";
+import { app, database } from "../firebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+export default function NoteDetails({ ID }) {
+  useEffect(() => {
+    getSingleNote();
+  }, [ID]);
 
-export default function NoteDetails() {
+  const getSingleNote = async () => {
+    if (ID) {
+      const singleNote = doc(database, "notes", ID);
+      const data = await getDoc(singleNote);
+      console.log({ ...data.data(), id: data.id });
+    }
+  };
   return <></>;
 }
