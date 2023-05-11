896de744764e56fb41961aa29e73fadb44de286d
RasoulDebugger <rasoul.merzai123@gmail.com>
2023-05-11 10:25:14 +0500

    useState for button visible

--- src/components/NoteOpreations.js
+++ src/components/NoteOpreations.js
@@ -1,10 +1,18 @@
 import styles from "../../styles/Evernote.module.scss";
+import { useState } from "react";
 
 export default function NoteOperations() {
+  const [isInputVisible, setInputVisible] = useState(false);
+  const inputToggle = () => {
+    setInputVisible(!isInputVisible);
+  };
+
   return (
     <>
       <div className={styles.btnContainer}>
-        <button className={styles.button}>Add a New Note</button>
+        <button onClick={inputToggle} className={styles.button}>
+          Add a New Note
+        </button>
       </div>
       <div className={styles.inputContainer}>
         <input className={styles.input} placeholder="Enter the Title.." />
