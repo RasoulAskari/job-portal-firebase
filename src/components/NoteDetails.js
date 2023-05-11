908678025e3cccad01489c28b1d9bda6ab9e487b
RasoulDebugger <rasoul.merzai123@gmail.com>
2023-05-12 00:49:30 +0500

    setSingleNote

--- src/components/NoteDetails.js
+++ src/components/NoteDetails.js
@@ -1,10 +1,12 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { app, database } from "../firebaseConfig";
 import { doc, getDoc } from "firebase/firestore";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 export default function NoteDetails({ ID }) {
+  const [singleNote, setSingleNote] = useState({});
+
   useEffect(() => {
     getSingleNote();
   }, [ID]);
@@ -13,7 +15,7 @@ export default function NoteDetails({ ID }) {
     if (ID) {
       const singleNote = doc(database, "notes", ID);
       const data = await getDoc(singleNote);
-      console.log({ ...data.data(), id: data.id });
+      setSingleNote({ ...data.date(), id: data.id });
     }
   };
   return <></>;
