21cc77185fcc28c24d32e63c2f971407b8cb419f
RasoulDebugger <rasoul.merzai123@gmail.com>
2023-05-14 10:40:01 +0500

    use firebase storage

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import firebase from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,6 +21,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const storage = firebase.storage();
+
 export const database = getFirestore(app);
 
 const analytics = getAnalytics(app);
